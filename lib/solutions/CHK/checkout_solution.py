e33ea013e66e102127d5bd5a174857854b02b12d
sbx_user1051 <sbx_user1051@169.254.50.87>
2025-04-02 00:47:07 +0000

    Wed Apr 02 00:47:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,7 +9,7 @@ def checkout(skus):
     
     prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35,
               "J": 60, "K": 70, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50,
-              "S": 20, "T": 20, "U": 40, "V": 50, "W": 20, "X": 17, "Y": 10, "Z": 50}
+              "S": 20, "T": 20, "U": 40, "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21}
     
     group_items = ["S", "T", "X", "Y", "Z"]
 
@@ -48,7 +48,7 @@ def checkout(skus):
     price += d["H"] * 10
     d["H"] = 0
 
-    price += (d["K"] // 2) * 150
+    price += (d["K"] // 2) * 120
     d["K"] %= 2
     price += d["K"] * 70
     d["K"] = 0
@@ -98,9 +98,3 @@ def checkout(skus):
             price += d[k] * prices[k]
     
     return price
-
-
-
-
-
-
