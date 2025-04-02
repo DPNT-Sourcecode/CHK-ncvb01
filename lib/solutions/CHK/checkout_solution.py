06294d36f84bf03b57bb89306534456ef2607583
sbx_user1051 <sbx_user1051@169.254.50.87>
2025-04-02 00:09:45 +0000

    Wed Apr 02 00:09:45 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    d = {"A": 0, "B": 0, "C": 0, "D": 0}
+    d = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
 
     for i in range(len(skus)):
         a = skus[i]
@@ -13,11 +13,13 @@ def checkout(skus):
             d[a] += 1
     
     price = 0
+    d["B"] -= d["E"] // 2
     price += d["C"] * 20
     price += d["D"] * 15
     price += ((d["A"] // 3) * 130) + ((d["A"] % 3) * 50)
     price += ((d["B"] // 2) * 45) + ((d["B"] % 2) * 30)
     return price
+
 
 
 
